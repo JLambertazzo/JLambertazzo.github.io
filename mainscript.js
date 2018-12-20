95162898cf11eea1c23ff0d314287726a68ed1df
JLambertazzo <42924425+JLambertazzo@users.noreply.github.com>
2018-12-20 00:15:50 -0600

    section headers grow/shrink

--- mainscript.js
+++ mainscript.js
@@ -3,11 +3,35 @@ $(document).ready(()=>{
     window.open("shooting.html", "_self");
   })
 
+  $("#shoot").on("mouseenter", (event)=>{
+    $(event.currentTarget).animate({fontSize: "2.5em"})
+  })
+
+  $("#shoot").on("mouseleave", (event)=>{
+    $(event.currentTarget).animate({fontSize: "2em"})
+  })
+
   $("#ball").on("click", ()=>{
     window.open("ballhandling.html", "_self");
   })
 
+  $("#ball").on("mouseenter", (event)=>{
+    $(event.currentTarget).animate({fontSize: "2.5em"})
+  })
+
+  $("#ball").on("mouseleave", (event)=>{
+    $(event.currentTarget).animate({fontSize: "2em"})
+  })
+
   $("#pass").on("click", ()=>{
     window.open("passing.html", "_self");
+  })
+
+  $("#pass").on("mouseenter", (event)=>{
+    $(event.currentTarget).animate({fontSize: "2.5em"})
+  })
+
+  $("#pass").on("mouseleave", (event)=>{
+    $(event.currentTarget).animate({fontSize: "2em"})
   })
 })
