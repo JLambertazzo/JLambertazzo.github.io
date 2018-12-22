c2ae3b633f0c6f04a83f600ab639cf2569121be7
JLambertazzo <42924425+JLambertazzo@users.noreply.github.com>
2018-12-22 04:15:00 -0600

    Simplified jQuery code

--- mainscript.js
+++ mainscript.js
@@ -3,35 +3,19 @@ $(document).ready(()=>{
     window.open("shooting.html", "_self");
   })
 
-  $("#shoot").on("mouseenter", (event)=>{
-    $(event.currentTarget).animate({fontSize: "2.5em"})
-  })
-
-  $("#shoot").on("mouseleave", (event)=>{
-    $(event.currentTarget).animate({fontSize: "2em"})
-  })
-
   $("#ball").on("click", ()=>{
     window.open("ballhandling.html", "_self");
-  })
-
-  $("#ball").on("mouseenter", (event)=>{
-    $(event.currentTarget).animate({fontSize: "2.5em"})
-  })
-
-  $("#ball").on("mouseleave", (event)=>{
-    $(event.currentTarget).animate({fontSize: "2em"})
   })
 
   $("#pass").on("click", ()=>{
     window.open("passing.html", "_self");
   })
 
-  $("#pass").on("mouseenter", (event)=>{
+  $(".subHeader").on("mouseenter", (event)=>{
     $(event.currentTarget).animate({fontSize: "2.5em"})
   })
 
-  $("#pass").on("mouseleave", (event)=>{
+  $(".subHeader").on("mouseleave", (event)=>{
     $(event.currentTarget).animate({fontSize: "2em"})
   })
 })
