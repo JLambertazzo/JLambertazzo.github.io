b5720e9074c45f05bdd5f18d369c3397286becdb
Julien <jbertazzolambert@gmail.com>
2019-03-27 01:26:44 -0500

    Made Space Invaders
    lots of changes from codepen lmaoooo

--- mainscript.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(()=>{
-  $("#inviteText").animate({ opacity: "0.8" }, 1000, "linear");
-  $("#mainTitle").animate({ opacity: "1" }, 1000, "linear");
-  $("#subTitle").animate({ opacity: "1" }, 1000, "linear");
-})
