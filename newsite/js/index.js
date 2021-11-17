3a8a3d406353f1bace5800fc5ef1201edd6d42d3
Julien <jbertazzolambert@gmail.com>
2021-11-17 00:51:12 -0500

    new site in progress

---
+++ newsite/js/index.js
@@ -0,0 +1,4 @@
+const myList = new ListExtender();
+myList.appendTo('#list-container');
+myList.setPlaceholder('Type Here!');
+myList.setInputType('text');
---
+++ newsite/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./*.html", "./*.json", "./js/*"],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
