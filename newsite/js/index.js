801f60161823d04529d8c5f409e12648ee89ac64
Julien <jbertazzolambert@gmail.com>
2021-11-20 00:48:03 -0500

    improved looks more - almost there

--- newsite/js/index.js
+++ newsite/js/index.js
@@ -2,3 +2,7 @@ const myList = new ListExtender();
 myList.appendTo('#list-container');
 myList.setPlaceholder('Type Here!');
 myList.setInputType('text');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rm = new RainMachine('.rain-container', { precipitation: Math.floor(Math.random() * 2) > 0 ? 'rain' : 'snow', wind: Math.floor(Math.random() * 3) })
+})
