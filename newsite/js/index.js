5b34ccdf4359514af6a86ed71e8a15b0407beba0
Julien <jbertazzolambert@gmail.com>
2021-11-25 22:41:13 -0500

    still trying lol

--- newsite/js/index.js
+++ newsite/js/index.js
@@ -1,8 +1,7 @@
-const myList = new ListExtender();
-myList.appendTo('#list-container');
-myList.setPlaceholder('Type Here!');
-myList.setInputType('text');
-
 document.addEventListener('DOMContentLoaded', () => {
     const rm = new RainMachine('.rain-container', { precipitation: Math.floor(Math.random() * 2) > 0 ? 'rain' : 'snow', wind: Math.floor(Math.random() * 3) })
+    const myList = new ListExtender();
+    myList.appendTo('#list-container');
+    myList.setPlaceholder('Type Here!');
+    myList.setInputType('text');
 })
