ae447058ec74ccd041ad0e869d54a085041a5c38
Julien <jbertazzolambert@gmail.com>
2021-12-01 20:33:54 -0500

    updated fetch urls

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://salty-brook-12944.herokuapp.com/weather/Toronto').then(res => res.json()).then(json => {
+    fetch('https://rainmachinejs.herokuapp.com/weather/Toronto').then(res => res.json()).then(json => {
         const rm = new RainMachine('.rain-container', json)
         document.querySelector('#weather-loading').classList.add('hidden')
         document.querySelector('#weather-loaded').classList.remove('hidden')
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(el)
         })
     })
-    fetch('https://salty-brook-12944.herokuapp.com/weather/Winnipeg').then(res => res.json()).then(json => {
+    fetch('https://rainmachinejs.herokuapp.com/weather/Winnipeg').then(res => res.json()).then(json => {
         const rm2 = new RainMachine('.rain-container-2', json)
     })
     const myList = new ListExtender();
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function search(e) {
     const val = document.querySelector('#search').value
-    fetch(`https://salty-brook-12944.herokuapp.com/weather/${val}`).then(res => res.json()).then(json => {
+    fetch(`https://rainmachinejs.herokuapp.com/weather/${val}`).then(res => res.json()).then(json => {
         const rm2 = new RainMachine('.rain-container-2', json)
     })
 }
