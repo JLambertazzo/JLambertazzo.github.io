8cbd0ceccc3991cea19707fdeba2aac5cc1a8056
Julien <jbertazzolambert@gmail.com>
2023-10-06 19:04:59 -0400

    use API directly

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://rainmachinejs.herokuapp.com/weather/Toronto').then(res => res.json()).then(json => {
-        const rm = new RainMachine('.rain-container', json)
-        document.querySelector('#weather-loading').classList.add('hidden')
-        document.querySelector('#weather-loaded').classList.remove('hidden')
-        const colour = rm.getSkyColour()
-        // set all relevant colours
-        document.querySelectorAll('.sky-colour').forEach(el => {
-            el.style.color = colour
-            console.log(el)
-        })
-    })
-    fetch('https://rainmachinejs.herokuapp.com/weather/Winnipeg').then(res => res.json()).then(json => {
-        const rm2 = new RainMachine('.rain-container-2', json)
-    })
-    const myList = new ListExtender();
-    myList.appendTo('#list-container');
-    myList.setPlaceholder('Type Here!');
-    myList.setInputType('text');
-})
+const query = (q) =>
+  fetch(`https://api.openweathermap.org/data/2.5/weather/?appid=c20f1f400b486d5d180a0287d75cafb4&q=${q}`);
+const optionsFromResponse = (res) =>
+  res.json().then(RainMachine.toRainMachineOptions);
+
+document.addEventListener("DOMContentLoaded", () => {
+  query("Toronto")
+    .then(optionsFromResponse)
+    .then((options) => {
+      const rm = new RainMachine(".rain-container", options);
+      document.querySelector("#weather-loading").classList.add("hidden");
+      document.querySelector("#weather-loaded").classList.remove("hidden");
+      const colour = rm.getSkyColour();
+      // set all relevant colours
+      document.querySelectorAll(".sky-colour").forEach((el) => {
+        el.style.color = colour;
+        console.log(el);
+      });
+    });
+  query("Winnipeg")
+    .then(optionsFromResponse)
+    .then((options) => {
+      const rm2 = new RainMachine(".rain-container-2", options);
+    });
+  const myList = new ListExtender();
+  myList.appendTo("#list-container");
+  myList.setPlaceholder("Type Here!");
+  myList.setInputType("text");
+});
 
 function search(e) {
-    const val = document.querySelector('#search').value
-    fetch(`https://rainmachinejs.herokuapp.com/weather/${val}`).then(res => res.json()).then(json => {
-        const rm2 = new RainMachine('.rain-container-2', json)
-    })
+  const val = document.querySelector("#search").value;
+  query(val)
+    .then((res) => res.json())
+    .then((json) => {
+      const rm2 = new RainMachine(".rain-container-2", json);
+    });
 }
