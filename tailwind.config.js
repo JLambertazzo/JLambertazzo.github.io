b03fb68f0e7280102ce384c2380aba8453eb103f
Julien <jbertazzolambert@gmail.com>
2024-03-12 15:14:24 -0400

    refactor: tailwind components and minor wording changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ["./*.html", "./*.json", "./js/*"],
-  darkMode: 'media', // or 'media' or 'class'
+  content: ["./**/*.{html,js}"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
